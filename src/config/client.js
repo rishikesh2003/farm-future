1b4a8fc9371f41e088c92b969830c3954bdd9931
Rishikesh S <rkesh2003@gmail.com>
2022-03-05 22:52:15 +0530

    Update client.js

--- src/config/client.js
+++ src/config/client.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseURL = "https://ljevudecygxfoenmtszu.supabase.co";
+const supabaseURL = "";
 const pubKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxqZXZ1ZGVjeWd4Zm9lbm10c3p1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDU4NDc5NjUsImV4cCI6MTk2MTQyMzk2NX0.pPx8zooqQxdOuySAB0-0Q1N5d9l5qciYwzAF3euJEXU";
+  "";
 
 const supabase = createClient(supabaseURL, pubKey);
 
